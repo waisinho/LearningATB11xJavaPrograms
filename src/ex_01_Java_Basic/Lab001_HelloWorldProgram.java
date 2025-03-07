15ecf184afe9be17c6567b77410937ce30bdb864
Promode <pramoddutta_96@yahoo.com>
2025-03-07 08:01:44 +0530

    7th March

--- src/ex_01_Java_Basic/Lab001_HelloWorldProgram.java
+++ src/ex_01_Java_Basic/Lab001_HelloWorldProgram.java
@@ -3,5 +3,8 @@ package ex_01_Java_Basic;
 public class Lab001_HelloWorldProgram {
     public static void main(String[] args) {
         System.out.println("hello");
+        // Write a program of factorial
+
+
     }
 }
---
+++ src/ex_01_Java_Basic/Lab002.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basic;
+
+public class Lab002 {
+    public static void main(String[] args) {
+
+    }
+}
