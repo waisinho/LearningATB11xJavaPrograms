959f2e6dad999b149044ff7242aedcfeca96db99
Promode <pramoddutta_96@yahoo.com>
2025-03-10 07:49:10 +0530

    10th March  - Commit

---
+++ src/ex_01_Java_Basic/LocalFile.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basic;
+
+public class LocalFile {
+    // This the final code that I have written
+    public static void main(String[] args) {
+        System.out.println("Final");
+    }
+
+}
