ad6deeef3ddc959a55012a47314d1b73e392041d
Promode <pramoddutta_96@yahoo.com>
2025-03-10 08:06:27 +0530

    10th March  - All

--- src/ex_01_Java_Basic/CloudFile.java
+++ src/ex_01_Java_Basic/CloudFile.java
@@ -1,6 +1,6 @@
 package ex_01_Java_Basic;
 
-public class LocalFile {
+public class CloudFile {
     // This the final code that I have written
     public static void main(String[] args) {
         System.out.println("Final");
