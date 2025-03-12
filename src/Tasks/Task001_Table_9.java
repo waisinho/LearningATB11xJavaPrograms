1b38310a619d1bb86c5f4641980083c7b00cd25d
Promode <pramoddutta_96@yahoo.com>
2025-03-12 08:13:44 +0530

    12th March Solution TASk

---
+++ src/Tasks/Task001_Table_9.java
@@ -0,0 +1,12 @@
+package Tasks;
+
+public class Task001_Table_9 {
+    public static void main(String[] args) {
+        int num = 9;
+        System.out.printf("%d*1=%d\n",num,num*1);
+        System.out.printf("%d*2=%d\n",num,num*2);
+        System.out.printf("%d*3=%d\n",num,num*3);
+        System.out.println();
+        System.out.printf("%d*10=%d\n",num,num*10);
+    }
+}
